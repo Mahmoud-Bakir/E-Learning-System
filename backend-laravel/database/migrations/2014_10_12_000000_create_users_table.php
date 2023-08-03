bfba9cc552a3691b3ba242e4a889967309e67f25
Rayan <rayanemad508@gmail.com>
2023-08-03 19:54:05 +0300

    BE-created users and admin plus models

--- backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,20 +9,36 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('UserID');
+            $table->id();
             $table->unsignedBigInteger('UserType'); // The foreign key to user_types
-            $table->string('Username')->unique();
             $table->string('Email')->unique();
             $table->string('Password');
             $table->string('FirstName');
             $table->string('LastName');
-            // Add other user-related columns as needed
             $table->timestamps();
         });
+
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('role');
+            $table->string('extra')->nullable();
+        });
+
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('Email')->unique();
+            $table->string('Password');
+            $table->string('FirstName');
+            $table->string('LastName');
+            $table->timestamps();
+        });
+
     }
 
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists("user_types");
+        Schema::dropIfExists('admins');
     }
 }
