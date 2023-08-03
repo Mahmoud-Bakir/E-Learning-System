91b0eff4e0f2c66dbb7fcbdbd7f6f4755b1a4645
Rayan <rayanemad508@gmail.com>
2023-08-03 14:24:34 +0300

    BE-users migration

--- backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,29 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->id('UserID');
+            $table->unsignedBigInteger('UserType'); // The foreign key to user_types
+            $table->string('Username')->unique();
+            $table->string('Email')->unique();
+            $table->string('Password');
+            $table->string('FirstName');
+            $table->string('LastName');
+            // Add other user-related columns as needed
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
