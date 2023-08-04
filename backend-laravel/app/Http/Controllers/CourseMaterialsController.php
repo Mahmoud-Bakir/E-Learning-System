3f801bf47a47573f486723a696e65fdbc0679a27
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-08-04 12:51:33 +0300

    BE-Added getEnrolledCourses

--- backend-laravel/app/Http/Controllers/CourseMaterialsController.php
+++ backend-laravel/app/Http/Controllers/CourseMaterialsController.php
@@ -10,15 +10,15 @@ class CourseMaterialsController extends Controller {
 
     function getCourseMaterials($courseId) {
 
-    $course = Course::find($courseId);
+        $course = Course::find($courseId);
 
-    if (!$course) {
-        return response()->json(['error' => 'Course not found.'], 404);
-    }
+        if (!$course) {
+            return response()->json(['error' => 'Course not found.'], 404);
+        }
 
-    $courseMaterials = CourseMaterial::where('course_id', $courseId)->get();
+        $courseMaterials = CourseMaterial::where('course_id', $courseId)->get();
 
-    return response()->json(['courseMaterials' => $courseMaterials]);
+        return response()->json(['courseMaterials' => $courseMaterials]);
 
     }
 
