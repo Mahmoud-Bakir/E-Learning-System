8eb67e8f38789f8d32a5b1cf0d1b46035a8b25af
Rayan <rayanemad508@gmail.com>
2023-08-04 12:46:05 +0300

    BE-fixed relationships and changed naming

---
+++ backend-laravel/app/Models/Assignment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    use HasFactory;
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'CourseID');
+    }
+    
+    public function submissions()
+    {
+        return $this->hasMany(Submission::class, 'AssignmentID');
+    }
+}
