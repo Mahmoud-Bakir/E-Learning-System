2376999d9b5f9c450eb8e6b46e535d8352aa37bf
Rayan <rayanemad508@gmail.com>
2023-08-12 14:33:04 +0300

    BE <> added admin update user controller

--- backend-laravel/app/Http/Controllers/AdminController.php
+++ backend-laravel/app/Http/Controllers/AdminController.php
@@ -5,28 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 use App\Models\User;
+use App\Models\Course;
 
 
-class AdminController extends Controller
-{
-    
-    public function register(Request $request){
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+class AdminController extends Controller{
+
+    public function updateUser(Request $request){
+        $user_id = $request->user_id;
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'string',
+            'last_name' => 'string',
+            'email' => 'email|unique:users,email',
+            'user_type' => 'exists:user_types,id',
         ]);
-        $user = new User; 
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->user_type_id = $request->user_type_id;
-        $user->save();
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::find($user_id);
+        if(!$user){return response() -> json(['Error' => 'No user found'],404);}
+
+        $user->update($request->only(['first_name', 'last_name', 'email', 'user_type']));
+
         return response()->json([
-            'status' => 'Success',
-            'data' => $user
+            'status' => 'success',
+            'message' => 'User account updated successfully',
+            'data' => [
+                'user_id' => $user_id,
+                'user' => $user,
+            ],
         ]);
     }
+
+
+    
 }
