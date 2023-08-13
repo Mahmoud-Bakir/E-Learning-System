9f346f7d98bff13c2ba9b91922cf3c0768635070
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-08-14 02:25:43 +0300

    student-fronten <> Added Chart Tab Component

--- frontend-react/src/pages/Parent/Course info/index.jsx
+++ frontend-react/src/pages/Parent/Course info/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './styles.css'
+import Tabs from '../../../Components/ParentComponents/Tabs'
+import Chart from '../../../Components/ParentComponents/Chart'
 
 function CourseInfo() {
+
+  const [filter, setFilter] = useState("all");
+  const data = [
+    {
+      "Section": "Average Grade",
+      "Average": 13,
+      "AverageColor": "hsl(0, 70%, 50%)",
+    },
+    {
+      "Section": "Submitted Assignments",
+      "Submitted Assignments": 148,
+      "AverageColor": "hsl(120, 70%, 50%)",
+    },
+    {
+      "Section": "Attendance",
+      "Attendance": 152,
+      "AverageColor": "hsl(240, 70%, 50%)",
+    },
+    {
+      "Section": "Progress",
+      "Progress": 42,
+      "AverageColor": "hsl(30, 70%, 50%)",
+    },
+  ]
+  
   return (
-    <div>Course Info</div>
+    <div className='parent-container'>
+    <Tabs onTabChanged={(value) => setFilter(value)}/>
+    <div className='chart'><Chart data = {data}/></div>
+    
+    </div>
+    
   )
 }
 
--- frontend-react/src/pages/Parent/Landing/index.jsx
+++ frontend-react/src/pages/Parent/Landing/index.jsx
@@ -3,14 +3,12 @@ import './styles.css'
 import DisplayList from '../../../Components/ParentComponents/DisplayList/index'
 import DataCard from '../../../Components/ParentComponents/DataCard'
 import TitleHeader from '../../../Components/ParentComponents/TitleHeader'
-import Tabs from '../../../Components/ParentComponents/Tabs'
+
 
 function Parent() {
 
-const [filter, setFilter] = useState("all");
   return (
-    <div className='flex column center parent-container'>
-      <Tabs onTabChanged={(value) => setFilter(value)}/>
+    <div className='flex column center parent-container'>  
       <div className='flex'>
         <DisplayList/>
         <div>
