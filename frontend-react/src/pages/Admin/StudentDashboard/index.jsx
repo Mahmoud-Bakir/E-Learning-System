ade9b358aff7f320a86abf7210003d2388c2d01c
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-15 00:22:27 +0300

    FE <> Created AdminNAv and SideMenu Compomonents

---
+++ frontend-react/src/pages/Admin/StudentDashboard/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminNav from '../../../Components/AdminComponents/AdminNav'
+import SideMenu from '../../../Components/AdminComponents/SideMenu'
+ const TeacherDashboard = () => {
+  return (
+    <>
+    <AdminNav/>
+    <div className='flex fullwidth fullheight '>
+      <SideMenu/>
+     
+
+    </div>
+    </>
+  )
+}
+ export default TeacherDashboard
