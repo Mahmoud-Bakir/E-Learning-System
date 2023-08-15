79a44b66ab5e0579bb327db98161e14cc72b451d
Rayan <rayanemad508@gmail.com>
2023-08-16 02:00:49 +0300

    teacher-frontend <> added assignments elements

--- frontend-react/src/Components/TeacherComponents/Assignments/index.jsx
+++ frontend-react/src/Components/TeacherComponents/Assignments/index.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
+import Notification from '../../Common/Notification';
 
-function Assignments() {
-  return (
-    <div>Assignments</div>
-  )
+function Assignments({assignments}) {
+  return (<>
+    <div className='assignments-cont'>
+      {assignments.map((assignment) => (
+        <Notification key={assignment.id} title={assignment.title} due={assignment.due_date} type='assignment' />
+      ))}
+    </div>
+  </>)
 }
 
 export default Assignments;
