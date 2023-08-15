bd1a53f88ebac7e919e8dd7a1a699ac875de39c9
Rayan <rayanemad508@gmail.com>
2023-08-15 11:57:27 +0300

    teacher-frontend <> Added Fetch Course Elements

--- frontend-react/src/Components/TeacherComponents/Class/index.jsx
+++ frontend-react/src/Components/TeacherComponents/Class/index.jsx
@@ -4,10 +4,35 @@ import MyResponsivePie from '../../Common/PieChart';
 import Notification from '../../Common/Notification';
 import Meeting from '../MeetingTab4';
 
+function Class({course_id}) {
+  const [assignments, setAssignments] = useState([]);
+  const [materials, setMaterials] = useState([]);
+  const token = localStorage.getItem('token');
 
-function Class() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/Teacher/course_elements', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id: course_id }),
+        });
 
-  const data =[ 
+        const data = await response.json();
+        setAssignments(data.assignments);
+        setMaterials(data.materials);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, [course_id, token]);
+
+  const data = [
     {
       "id": "Attendence",
       "label": "Attendence",
@@ -20,33 +45,29 @@ function Class() {
       "value": 296,
       "color": "hsl(168, 70%, 50%)"
     },
-  ]
+  ];
 
-  return (<>
-
+  return (
     <div className='class-content'>
       <div className='left-content'>
-        
         <div className='MyResponsivePie'>
-          <MyResponsivePie  data={data}/>
+          <MyResponsivePie data={data} />
         </div>
+        <Meeting url={'akskaks'} title={'Class Link'} />
+        <Meeting url={'akskaks'} title={'Calendly Link'} />
+      </div>
 
-      <Meeting url={'akskaks'} title={'Class Link'}/>
-      <Meeting url={'akskaks'} title={'Calendly Link'}/>
+      <div className='right-content'>
+        {materials.map((material) => (
+          <Notification key={material.id} title={material.title} type="Material" />
+        ))}
+        {assignments.map((assignment) => (
+          <Notification key={assignment.id} title={assignment.title} due={assignment.due_date} type="assignment"/>
+        ))}
         
       </div>
- 
-      <div className='right-content'>
-        <Notification title="Math: Uploaded New Material" type="Matrial" />
-        <Notification title="Math" due="14-May" type="assignment" />
-        <Notification title="Math" type="Matrial" />
-      </div>
-
     </div>
-    
-    
-    
-  </>)
+  );
 }
 
-export default Class
+export default Class;
