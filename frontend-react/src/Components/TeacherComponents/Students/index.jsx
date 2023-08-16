6119d18796284ff26ed70eac89c03550a4db0281
Rayan <rayanemad508@gmail.com>
2023-08-16 08:36:30 +0300

    FE <> added fetch for students

--- frontend-react/src/Components/TeacherComponents/Students/index.jsx
+++ frontend-react/src/Components/TeacherComponents/Students/index.jsx
@@ -1,10 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import StudentCard from '../StudentCard'
 import './styles.css';
 
-function Students() {
-  return (
-    <div>Students</div>
-  )
+function Students({course_id}) {
+  const [courseData, setCourseData] = useState({});
+  const [studentAnalytics, setStudentAnalytics] = useState([]);
+
+  useEffect(() => {
+    fetchStudentAnalytics();
+  }, []);
+
+  const fetchStudentAnalytics = () => {
+    fetch('http://127.0.0.1:8000/api/Admin/course_students_analytics', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ course_id: course_id }),
+    })
+      .then((response) => {return response.json();})
+      .then((data) => {
+        setCourseData(data.course);
+        setStudentAnalytics(data.student_analytics);
+      })
+      .catch((error) => {
+        console.error('Error fetching student analytics:', error);
+      });
+  };
+
+  return (<>
+      <h2 className="course-title">Course: {courseData.course_name}</h2>
+      <div className="students-container">
+          {studentAnalytics.map((studentInfo) => (
+            <StudentCard key={studentInfo.student.id} studentInfo={studentInfo} />
+          ))}
+      </div>
+  </>);
 }
 
-export default Students
+export default Students;
